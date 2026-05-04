7d76b9a761fc9edbf88b1dff711ffda372f7231d
AliannahNF <aen00010@mix.wvu.edu>
2026-05-04 02:29:13 -0400

    Add procedural key spawning, furniture, exit door, final scene

--- Assets/Scripts/KeyItem.cs
+++ Assets/Scripts/KeyItem.cs
@@ -4,6 +4,16 @@ public class KeyItem : MonoBehaviour
 {
     [SerializeField] private string keyID = "daughterKey";
     [SerializeField] private GameObject pickupEffect;
+    [SerializeField] private Transform[] spawnPoints;
+
+    private void Start()
+    {
+        if (spawnPoints != null && spawnPoints.Length > 0)
+        {
+            int random = Random.Range(0, spawnPoints.Length);
+            transform.position = spawnPoints[random].position;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
